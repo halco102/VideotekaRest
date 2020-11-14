01bec5000eac96b462dcdae054ecd177ae2448f4
halco102 <ado.halilovic@outlook.com>
2020-11-14 19:53:21 +0100

    commit

--- src/main/java/com/blockbusterREST/blockbusterRest/service/CountryService.java
+++ src/main/java/com/blockbusterREST/blockbusterRest/service/CountryService.java
@@ -1,4 +1,15 @@
 package com.blockbusterREST.blockbusterRest.service;
 
+import com.blockbusterREST.blockbusterRest.domain.Country;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
 public interface CountryService {
+
+    ResponseEntity<Country> saveCountry(Country country);
+    void deleteCountry(Long id);
+    ResponseEntity<List<Country>> getAllCountries();
+
+
 }
--- src/main/java/com/blockbusterREST/blockbusterRest/service/implementation/CountryServiceImpl.java
+++ src/main/java/com/blockbusterREST/blockbusterRest/service/implementation/CountryServiceImpl.java
@@ -1,8 +1,36 @@
 package com.blockbusterREST.blockbusterRest.service.implementation;
 
+import com.blockbusterREST.blockbusterRest.domain.Country;
+import com.blockbusterREST.blockbusterRest.repositories.CountryRepository;
 import com.blockbusterREST.blockbusterRest.service.CountryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CountryServiceImpl implements CountryService {
+    private CountryRepository countryRepository;
+
+    public CountryServiceImpl(CountryRepository countryRepository) {
+        this.countryRepository = countryRepository;
+    }
+
+    @Override
+    public ResponseEntity<Country> saveCountry(Country country) {
+        Country country1 = this.countryRepository.save(country);
+        return new ResponseEntity<Country>(country1, HttpStatus.OK);
+    }
+
+    @Override
+    public void deleteCountry(Long id) {
+        this.countryRepository.deleteById(id);
+    }
+
+    @Override
+    public ResponseEntity<List<Country>> getAllCountries() {
+        List<Country> countries = this.countryRepository.findAll();
+        return new ResponseEntity<List<Country>>(countries,HttpStatus.OK);
+    }
 }
